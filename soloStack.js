d39dda3017a78d1dcd23d0b22010733e8e0e7713
peaches <pablo.chois@gmail.com>
2018-01-03 09:48:31 -0500

    updated queries
    updated queries to only return the required info: id and name

--- soloStack.js
+++ soloStack.js
@@ -24,7 +24,7 @@ const retrieveMakes = (req, res, next) => {
 // error handling is very basic
 // for this call, there should never be a case where this query returns no results -- not handling a case if it does
 const retrieveModels = (req, res, next) => {
-  db.all("SELECT * FROM Models m JOIN MakeModelMap mmm on m.id = mmm.model_id WHERE mmm.make_id = $id ORDER BY name ASC;",
+  db.all("SELECT model_id, name FROM Models m JOIN MakeModelMap mmm on m.id = mmm.model_id WHERE mmm.make_id = $id ORDER BY name ASC;",
   {$id: req.query.makeId},
   (err, models) => {
     if(err){
@@ -39,7 +39,7 @@ const retrieveModels = (req, res, next) => {
 // error handling is very basic
 // for this call, currently not handling the case where no results are returned -- look into returning a 204
 const retrieveTrims = (req, res, next) => {
-  db.all("SELECT * from Trims t JOIN ModelTrimMap mtm on t.id = mtm.trim_id WHERE mtm.make_id = $make_id AND mtm.model_id = $model_id ORDER BY name ASC;",
+  db.all("SELECT trim_id, name from Trims t JOIN ModelTrimMap mtm on t.id = mtm.trim_id WHERE mtm.make_id = $make_id AND mtm.model_id = $model_id ORDER BY name ASC;",
   {$make_id: req.query.makeId, $model_id: req.query.modelId},
   (err, trims) => {
     if(err){
@@ -54,7 +54,7 @@ const retrieveTrims = (req, res, next) => {
 // error handling is very basic
 // for this call, there should never be a case where this query returns no results -- not handling a case if it does
 const retrieveYears = (req, res, next) => {
-  db.all("SELECT * FROM Years y JOIN TrimYearMap tym ON y.id = tym.year_id WHERE tym.make_id = $make_id AND tym.model_id = $model_id AND tym.trim_id = $trim_id ORDER BY name ASC;",
+  db.all("SELECT year_id, name FROM Years y JOIN TrimYearMap tym ON y.id = tym.year_id WHERE tym.make_id = $make_id AND tym.model_id = $model_id AND tym.trim_id = $trim_id ORDER BY name ASC;",
   {$make_id: req.query.makeId, $model_id: req.query.modelId, $trim_id: req.query.trimId},
   (err, years) => {
     if(err){
@@ -69,7 +69,7 @@ const retrieveYears = (req, res, next) => {
 // error handling is very basic
 // for this call, there should never be a case where this query returns no results -- not handling a case if it does
 const retrieveYearsS3 = (req, res, next) => {
-  db.all("SELECT * FROM Years y JOIN ModelYearMap mym ON y.id = mym.year_id WHERE mym.make_id = $make_id AND mym.model_id = $model_id ORDER BY name ASC;",
+  db.all("SELECT year_id, name FROM Years y JOIN ModelYearMap mym ON y.id = mym.year_id WHERE mym.make_id = $make_id AND mym.model_id = $model_id ORDER BY name ASC;",
   {$make_id: req.query.makeId, $model_id: req.query.modelId},
   (err, yearss3) => {
     if(err){
@@ -85,7 +85,7 @@ const retrieveYearsS3 = (req, res, next) => {
 // for this call, there should never be a case where this query returns no results -- not handling a case if it does
 // currently using db.all, should really use db.get since only 1 result should be returned. Maybe leave db.all and handle the error is more than 1 result comes back.
 const retrieveClassS1 = (req, res, next) => {
-  db.all("SELECT * FROM Classes c JOIN Scenario1 s1 ON c.id = s1.class_id WHERE s1.make_id = $make_id AND s1.model_id = $model_id ORDER BY name ASC;",
+  db.all("SELECT class_id, name FROM Classes c JOIN Scenario1 s1 ON c.id = s1.class_id WHERE s1.make_id = $make_id AND s1.model_id = $model_id ORDER BY name ASC;",
   {$make_id: req.query.makeId, $model_id: req.query.modelId},
   (err, classes1) => {
     if(err){
@@ -101,7 +101,7 @@ const retrieveClassS1 = (req, res, next) => {
 // for this call, there should never be a case where this query returns no results -- not handling a case if it does
 // currently using db.all, should really use db.get since only 1 result should be returned. Maybe leave db.all and handle the error is more than 1 result comes back.
 const retrieveClassS2 = (req, res, next) => {
-  db.all("SELECT * FROM Classes c JOIN Scenario2 s2 ON c.id = s2.class_id WHERE s2.make_id = $make_id AND s2.model_id = $model_id AND s2.trim_id = $trim_id ORDER BY name ASC;",
+  db.all("SELECT class_id, name FROM Classes c JOIN Scenario2 s2 ON c.id = s2.class_id WHERE s2.make_id = $make_id AND s2.model_id = $model_id AND s2.trim_id = $trim_id ORDER BY name ASC;",
   {$make_id: req.query.makeId, $model_id: req.query.modelId, $trim_id: req.query.trimId},
   (err, classes2) => {
     if(err){
@@ -117,7 +117,7 @@ const retrieveClassS2 = (req, res, next) => {
 // for this call, there should never be a case where this query returns no results -- not handling a case if it does
 // currently using db.all, should really use db.get since only 1 result should be returned. Maybe leave db.all and handle the error is more than 1 result comes back.
 const retrieveClassS3 = (req, res, next) => {
-  db.all("SELECT * FROM Classes c JOIN Scenario3 s3 ON c.id = s3.class_id WHERE s3.make_id = $make_id AND s3.model_id = $model_id AND s3.year_id = $year_id ORDER BY name ASC;",
+  db.all("SELECT class_id, name FROM Classes c JOIN Scenario3 s3 ON c.id = s3.class_id WHERE s3.make_id = $make_id AND s3.model_id = $model_id AND s3.year_id = $year_id ORDER BY name ASC;",
   {$make_id: req.query.makeId, $model_id: req.query.modelId, $year_id: req.query.yearId},
   (err, classes3) => {
     if(err){
@@ -133,7 +133,7 @@ const retrieveClassS3 = (req, res, next) => {
 // for this call, there should never be a case where this query returns no results -- not handling a case if it does
 // currently using db.all, should really use db.get since only 1 result should be returned. Maybe leave db.all and handle the error is more than 1 result comes back.
 const retrieveClassS4 = (req, res, next) => {
-  db.all("SELECT * FROM Classes c JOIN Scenario4 s4 ON c.id = s4.class_id WHERE s4.make_id = $make_id AND s4.model_id = $model_id AND s4.trim_id = $trim_id AND s4.year_id = $year_id ORDER BY name ASC;",
+  db.all("SELECT class_id, name FROM Classes c JOIN Scenario4 s4 ON c.id = s4.class_id WHERE s4.make_id = $make_id AND s4.model_id = $model_id AND s4.trim_id = $trim_id AND s4.year_id = $year_id ORDER BY name ASC;",
   {$make_id: req.query.makeId, $model_id: req.query.modelId, $trim_id: req.query.trimId, $year_id: req.query.yearId},
   (err, classes4) => {
     if(err){
